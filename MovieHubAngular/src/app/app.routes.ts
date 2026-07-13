d90a2f69b4f4e338f81f5a4734be11295659eda8
Carlos P. <dawcarlospereira@gmail.com>
2026-07-13 20:10:27 +0200

    feat(routes): añadir ruta /login con lazy loading para LoginPageComponent

--- MovieHubAngular/src/app/app.routes.ts
+++ MovieHubAngular/src/app/app.routes.ts
@@ -3,5 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   { path: '', redirectTo: '/inicio', pathMatch: 'full' },
   { path: 'inicio', loadComponent: () => import('./features/home/home-page.component').then(c => c.HomePageComponent) },
-  { path: 'genero/:nombre', loadComponent: () => import('./features/genero/genero-page.component').then(c => c.GeneroPageComponent) }
+  { path: 'genero/:nombre', loadComponent: () => import('./features/genero/genero-page.component').then(c => c.GeneroPageComponent) },
+  { path: 'login', loadComponent: () => import('./features/auth/login-page.component').then(c => c.LoginPageComponent) }
 ];
