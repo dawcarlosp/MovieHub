b2c99c056210aaac940253de2b099d8ad03d66cb
Carlos P. <dawcarlospereira@gmail.com>
2026-07-14 15:53:49 +0200

    fix: actualizar import de interceptor tras merge con main

--- MovieHubAngular/src/app/app.config.ts
+++ MovieHubAngular/src/app/app.config.ts
@@ -4,14 +4,14 @@ import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
-import { errorInterceptor } from './core/interceptors/error.interceptor';
+import { authErrorInterceptor } from './core/interceptors/auth-error.interceptor';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes, withComponentInputBinding()),
-    provideHttpClient(withFetch(), withInterceptors([errorInterceptor, authInterceptor])),
+    provideHttpClient(withFetch(), withInterceptors([authErrorInterceptor, authInterceptor])),
     provideAnimations(),
   ],
 };
